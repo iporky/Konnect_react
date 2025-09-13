af1552ce1fadaeca9d9cd7af7fbf505585708bbb
shivang <shivang.chauhan@247-inc.com>
2025-09-13 23:13:42 +0530

    browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { ThemeContextProvider } from './contexts/ThemeContext';
 import { useDispatch, useSelector } from 'react-redux';
